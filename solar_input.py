4dc4af8b539f7e1ae5c9f14cb7d082c67d00759e
Arseniy <plakhotniuk.av@phystech.edu>
2021-03-29 19:30:35 +0300

    read_space_objects_data_from_file, parse_planet_parameters, parse_star_parameters

--- solar_input.py
+++ solar_input.py
@@ -19,10 +19,14 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            if object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -43,8 +47,27 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+    star_massive = line.split()
+    for i in range(len(star_massive)):
+        if i == 0:
+            star.type = star[0]
+        if i == 1 or 2 < i <= 7:
+            star_massive[i] = int(star_massive[i])
+        if i == 2:
+            star.color = star[i]
+        if i == 1:
+            star.R = star_massive[i]
+        if i == 3:
+            star.m = star_massive[i]
+        if i == 4:
+            star.x = star_massive[i]
+        if i == 5:
+            star.y = star_massive[i]
+        if i == 6:
+            star.Vx = star_massive[i]
+        if i == 7:
+            star.Vy = star_massive[i]
 
-    pass  # FIXME: not done yet
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,7 +84,26 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    planet_massive = line.split()
+    for i in range(len(planet_massive)):
+        if i == 0:
+            planet.type = planet[0]
+        if i == 1 or 2 < i <= 7:
+            planet_massive[i] = int(planet_massive[i])
+        if i == 1:
+            planet.R = planet_massive[i]
+        if i == 2:
+            planet.color = planet[i]
+        if i == 3:
+            planet.m = planet_massive[i]
+        if i == 4:
+            planet.x = planet_massive[i]
+        if i == 5:
+            planet.y = planet_massive[i]
+        if i == 6:
+            planet.Vx = planet_massive[i]
+        if i == 7:
+            planet.Vy = planet_massive[i]
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
